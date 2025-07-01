a67c1f92450a14a4ff42c0491c29634f9759e48e
vino <shanvino.012710@gmail.com>
2025-06-30 21:45:57 -0400

    Master copy

---
+++ src/test/java/operators/Assignment.java
@@ -0,0 +1,21 @@
+package operators;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+	 
+	 int a=10;  
+	 a+=3;//a=a+3=(10+3) 
+	 
+	 System.out.println(a);  
+	  a-=4;//a=a-4=(13-4) 
+	 
+	 System.out.println(a);  
+	  a*=2;//a=a*2=(9*2) 
+	  
+	 System.out.println(a);  
+	 a/=2;//a=a/2=(18/2)
+	 
+	 System.out.println(a); 
+}
+}
